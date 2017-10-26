33ecf7dc6dccda964626e0de9808ccc9dd76a07e
Derek Bytheway <derekbtw@gmail.com>
2017-10-26 05:36:40 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,9 @@ chrome.app.runtime.onLaunched.addListener(function() {
     runApp();
 });
 
-
 chrome.app.runtime.onRestarted.addListener(function() {
     runApp();
 });
-
 
 function runApp() {
     chrome.app.window.create('app.html', {
